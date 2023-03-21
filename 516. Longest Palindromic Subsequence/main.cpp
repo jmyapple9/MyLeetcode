a40a7a33b125c0193a2bddf7efa753ef17f3e89c
108000232 <jmyapple9@gmail.com>
2023-03-21 14:35:16 +0800

    simplist

--- 516. Longest Palindromic Subsequence/main.cpp
+++ 516. Longest Palindromic Subsequence/main.cpp
@@ -5,21 +5,16 @@ using namespace std;
 class Solution
 {
 public:
-    int longestPalindromeSubseq(string s)
-    {
+    int longestPalindromeSubseq(string s){
         vector<vector<int>> DP(s.size(), vector<int>(s.size()));
-        int ans = 0;
-        for (int r = 0; r < s.size(); r++)
-        {   
-            for (int l = r; l>=0; l--)
-            {
+        for (int r = 0; r < s.size(); r++){   
+            for (int l = r; l>=0; l--){
                 if (r - l == 0)
                     DP[l][r] = 1;
                 else if (s[l] == s[r])
                     DP[l][r] = DP[l + 1][r - 1] + 2;
                 else
                     DP[l][r] = max(DP[l + 1][r], DP[l][r - 1]);
-                ans = max(ans, DP[l][r]);
             }
         }
         return DP[0][s.size()-1];
